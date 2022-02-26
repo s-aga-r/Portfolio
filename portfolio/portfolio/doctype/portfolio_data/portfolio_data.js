b0a6d2637cd614cf1954bc635a4250169e47ef08
Sagar Sharma <sagarsharma.s312@gmail.com>
2022-02-27 01:11:41 +0530

    feat: New DocType "Portfolio Data"

---
+++ portfolio/portfolio/doctype/portfolio_data/portfolio_data.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Sagar Sharma and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Portfolio Data', {
+	// refresh: function(frm) {
+
+	// }
+});
