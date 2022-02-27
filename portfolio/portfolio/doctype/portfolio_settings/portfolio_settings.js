1dec686c29c39df323dd025ef749e708ebbbb22d
Sagar Sharma <sagarsharma.s312@gmail.com>
2022-02-27 13:20:24 +0530

    feat: New DocType "Portfolio Settings"

---
+++ portfolio/portfolio/doctype/portfolio_settings/portfolio_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Sagar Sharma and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Portfolio Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
