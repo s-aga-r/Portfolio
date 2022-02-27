9ba3e08e7b3d6e3cc244bab0850ac4d0868970b6
Sagar Sharma <sagarsharma.s312@gmail.com>
2022-02-27 09:38:47 +0530

    Revert "feat: New DocType "Portfolio Category""
    This reverts commit cc43a60128dce0de91a03a64cbdc8b1520a93faa.

--- portfolio/portfolio/doctype/portfolio_category/portfolio_category.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2022, Sagar Sharma and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Portfolio Category', {
-	// refresh: function(frm) {
-
-	// }
-});
