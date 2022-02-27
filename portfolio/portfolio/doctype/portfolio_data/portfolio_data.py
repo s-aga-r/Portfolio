d86cb2ef629078b2f07002b697ee319d388e8405
Sagar Sharma <sagarsharma.s312@gmail.com>
2022-02-27 09:39:29 +0530

    Revert "feat: New DocType "Portfolio Data""
    This reverts commit b0a6d2637cd614cf1954bc635a4250169e47ef08.

--- portfolio/portfolio/doctype/portfolio_data/portfolio_data.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2022, Sagar Sharma and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class PortfolioData(Document):
-	pass
--- portfolio/portfolio/doctype/portfolio_data/test_portfolio_data.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2022, Sagar Sharma and Contributors
-# See license.txt
-
-# import frappe
-import unittest
-
-class TestPortfolioData(unittest.TestCase):
-	pass
