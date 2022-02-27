0c387d6e82400d7961189771a70cb4ea562919c3
Sagar Sharma <sagarsharma.s312@gmail.com>
2022-02-27 13:42:06 +0530

    fix: "is_active" filter in "Portfolio Items"

--- portfolio/www/portfolio.py
+++ portfolio/www/portfolio.py
@@ -8,24 +8,28 @@ def get_context(context):
     portfolio_names = frappe.db.get_all("Portfolio", {"is_active": 1})
 
     if portfolio_names:
+        
         for portfolio_name in portfolio_names:
             portfolio_doc = frappe.get_doc("Portfolio", portfolio_name.get("name"))
 
             for item in portfolio_doc.get("items"):
-                file_names = frappe.db.get_all("File", {"folder": item.get("folder"), "is_folder": 0, "is_private": 0})
 
-                items = []
+                if item.get("is_active"):
 
-                for file_name in file_names:
-                    items.append(frappe.db.get_value("File", file_name.get("name"), "file_url"))
+                    file_names = frappe.db.get_all("File", {"folder": item.get("folder"), "is_folder": 0, "is_private": 0})
 
-                result.append(
-                    {
-                        "category": portfolio_doc.get("category"),
-                        "filter": portfolio_doc.get("filter"),
-                        "items": items
-                    }
-                )
+                    items = []
+
+                    for file_name in file_names:
+                        items.append(frappe.db.get_value("File", file_name.get("name"), "file_url"))
+
+                    result.append(
+                        {
+                            "category": portfolio_doc.get("category"),
+                            "filter": portfolio_doc.get("filter"),
+                            "items": items
+                        }
+                    )
 
     context.data = result
     context.page_title = frappe.db.get_single_value("Portfolio Settings", "page_title")
